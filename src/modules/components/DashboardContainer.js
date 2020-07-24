276065090102ff16026e9c7ce74ef775ac8b588a
sunnyRK <radadiyasunny970@gmail.com.com>
2020-07-24 12:02:49 +0530

    added dashboard

---
+++ src/modules/components/DashboardContainer.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import Dashboard from './Dashboard';
+import MainTemplate from '../shared/main-template/MainTemplateContainer';
+import web3 from '../../../config/web3';
+import NetworkTypeDialogContainer from './NetworkTypeDialog/NetworkTypeDialogContainer';
+
+class DashboardContainer extends Component {
+  state = {
+    displayMessage: '',
+  };
+
+  async componentDidMount() {
+    let networkType;
+    await web3.eth.net.getNetworkType().then(function(type) {
+      networkType = type
+    });
+    console.log(networkType);
+    if(networkType != "kovan") {
+      // alert("Network Error: Change network " + networkType + " to rinkeby");
+      this.setState({
+        displayMessage: "Network Error: Change network " + networkType + " to kovan",
+      });
+    } else {
+      this.setState({
+        displayMessage: '',
+      });
+    }
+    this.enablewindow();
+  }
+
+  async enablewindow() {
+    await window.ethereum.enable();
+  }
+
+  render() {
+    return (
+      <MainTemplate>
+        <Dashboard />
+        <NetworkTypeDialogContainer
+          displayMessage={this.state.displayMessage}
+          openDialog={this.state.displayMessage}
+        />
+      </MainTemplate>
+    );
+  }
+}
+
+export default DashboardContainer;
