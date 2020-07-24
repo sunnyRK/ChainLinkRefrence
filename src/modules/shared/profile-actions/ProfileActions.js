d8ecc7c57c8b548c39c0c60d21a923f8e8dc6d5a
sunnyRK <radadiyasunny970@gmail.com.com>
2020-07-24 12:03:44 +0530

    Added Shared module

---
+++ src/modules/shared/profile-actions/ProfileActions.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {
+  InputLabel, MenuItem, FormControl, Select,
+} from '@material-ui/core';
+import BlockUI from 'react-block-ui';
+
+import GoogleLoader from '../GoogleLoader';
+import TransactionHistory from './TransactionHistory';
+// import NetworkTypeDialogContainer from '../../components/NetworkTypeDialog/NetworkTypeDialogContainer';
+
+const ProfileActions = ({
+  metamaskAddress, checkPairAddress, symbol0, symbol1,
+  checkPairBalance, reserve0, reserve1, options, handlecheckPairs, Lblocking, Tblocking
+}) => (
+  <div className="profile-actions">
+    
+    {/* <NetworkTypeDialogContainer
+          displayMessage={this.state.displayMessage}
+          openDialog={this.state.displayMessage}
+        /> */}
+    <BlockUI
+      tag="div"
+      blocking={Tblocking}
+      loader={<GoogleLoader height={50} width={50} />}
+    >
+      <div className="card">
+        <h3>Metamask address</h3>
+        <div className="address">{metamaskAddress || '0x....'}</div>
+      </div>
+    </BlockUI>
+
+    <BlockUI
+      tag="div"
+      blocking={Lblocking}
+      loader={<GoogleLoader height={50} width={50} />}
+    >
+      <div className="check-liquidity card">
+        <h3>Liquidity Check</h3>
+        <FormControl fullWidth variant="outlined" classes={{ root: 'check-liquidity-dropdown' }}>
+          <InputLabel>Select Pair</InputLabel>
+          <Select
+            value={checkPairAddress}
+            onChange={handlecheckPairs}
+            label="Select Pair"
+          >
+            {
+              options.map((option) => (
+                <MenuItem value={option.value}>{option.text}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+        <div className="card">
+          <h4>Your {checkPairAddress} Pool token</h4>
+          <div className="address">{checkPairBalance} Uni-V2 wei</div>
+        </div>
+        <div className="card">
+          <h4>Total {symbol0} liquidity</h4>
+          <div className="address">{reserve0} wei</div>
+        </div>
+        <div className="card">
+          <h4>Total {symbol1} liquidity</h4>
+          <div className="address">{reserve1} wei</div>
+        </div>
+      </div>
+    </BlockUI>
+
+    <BlockUI
+      tag="div"
+      blocking={Tblocking}
+      loader={<GoogleLoader height={50} width={50} />}
+    >
+      <div className="transaction-history-wrapper card">
+        <h3>Transaction History</h3>
+        <TransactionHistory />
+      </div>
+    </BlockUI>
+  </div>
+);
+
+export default ProfileActions;
---
+++ src/modules/shared/profile-actions/ProfileActionsContainer.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react';
+
+import ProfileActions from './ProfileActions';
+// import { Button, Form, Segment, Grid, Dropdown, Message, Image, Statistic, Divider } from 'semantic-ui-react';
+import web3 from '../../../../config/web3';
+import {
+  getERCContractInstance,
+  getUniswapV2Pair,
+  PairInfoArray,
+  getERCContractInstanceWithoutSymbol,
+  tagOptions,
+} from '../../../../config/instances/contractinstances';
+
+class ProfileActionsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkPairAddress: 'DAI-LINK',
+      checkPairBalance: '0',
+      symbol0: '',
+      symbol1: '',
+      reserve0: '0',
+      reserve1: '0',
+      metamaskAddress: '',
+      Lblocking: false,
+      Tblocking: false
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ Tblocking: true });
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+    if(accounts.length > 0) {
+      const UniV2PairAddress = this.state.checkPairAddress;
+      const pairInstance = await getUniswapV2Pair(web3, PairInfoArray[0][this.state.checkPairAddress].pairaddress);
+
+      const erc20ContractInstance1 = await getERCContractInstance(web3, UniV2PairAddress);
+
+      const reserves = await pairInstance.methods.getReserves().call();
+
+      const token0 = await pairInstance.methods.token0().call();
+      const token1 = await pairInstance.methods.token1().call();
+
+      const token0Instance = await getERCContractInstanceWithoutSymbol(web3, token0);
+      const token1Instance = await getERCContractInstanceWithoutSymbol(web3, token1);
+
+      const symbol0 = await token0Instance.methods.symbol().call();
+      const symbol1 = await token1Instance.methods.symbol().call();
+
+      const poolTokenBalance = await erc20ContractInstance1.methods.balanceOf(accounts[0]).call();
+
+      const poolBal = (parseFloat(poolTokenBalance) / 1000000000000000000).toFixed(2);
+      const rese0 = (parseFloat(reserves[0]) / 1000000000000000000).toFixed(2);
+      const rese1 = (parseFloat(reserves[1]) / 1000000000000000000).toFixed(2);
+
+      const pool0 = `${poolTokenBalance}(${poolBal})`;
+      const res0 = `${reserves[0]}(${rese0})`;
+      const res1 = `${reserves[1]}(${rese1})`;
+
+      this.setState({
+        checkPairBalance: pool0,
+        reserve0: res0,
+        reserve1: res1,
+        symbol0,
+        symbol1,
+        metamaskAddress: accounts[0],
+        Tblocking: false
+      });
+    }
+  }
+
+  checkPoolTokenPair = async () => {
+    event.preventDefault();
+    try {
+      this.setState({ Lblocking: true });
+      const accounts = await web3.eth.getAccounts();
+      // if(accounts.length > 0) {
+        const UniV2PairAddress = this.state.checkPairAddress;
+        const pairInstance = await getUniswapV2Pair(web3, PairInfoArray[0][this.state.checkPairAddress].pairaddress);
+        const reserves = await pairInstance.methods.getReserves().call();
+
+        const token0 = await pairInstance.methods.token0().call();
+        const token1 = await pairInstance.methods.token1().call();
+
+        const token0Instance = await getERCContractInstanceWithoutSymbol(web3, token0);
+        const token1Instance = await getERCContractInstanceWithoutSymbol(web3, token1);
+
+        const symbol0 = await token0Instance.methods.symbol().call();
+        const symbol1 = await token1Instance.methods.symbol().call();
+
+        const erc20ContractInstance1 = await getERCContractInstance(web3, UniV2PairAddress);
+        const poolTokenBalance = await erc20ContractInstance1.methods.balanceOf(accounts[0]).call();
+
+        const poolBal = (parseFloat(poolTokenBalance) / 1000000000000000000).toFixed(2);
+        const rese0 = (parseFloat(reserves[0]) / 1000000000000000000).toFixed(2);
+        const rese1 = (parseFloat(reserves[1]) / 1000000000000000000).toFixed(2);
+
+        const pool0 = `${poolTokenBalance}(${poolBal})`;
+        const res0 = `${reserves[0]}(${rese0})`;
+        const res1 = `${reserves[1]}(${rese1})`;
+
+        this.setState({
+          checkPairBalance: pool0,
+          reserve0: res0,
+          reserve1: res1,
+          symbol0,
+          symbol1,
+        });
+        this.setState({ Lblocking: false });
+      // }
+    } catch (error) {
+      this.setState({ Lblocking: false });
+      console.log(error);
+    }
+  };
+
+  handlecheckPairs = (e) => {
+    this.setState({
+      checkPairAddress: e.target.value,
+    }, () => this.checkPoolTokenPair());
+  };
+
+  render() {
+    const { reserve1, reserve0, symbol1, symbol0, checkPairBalance, checkPairAddress, 
+      metamaskAddress, Tblocking, Lblocking }
+     = this.state;
+    return (
+      <ProfileActions
+        options={tagOptions}
+        reserve1={reserve1}
+        reserve0={reserve0}
+        symbol1={symbol1}
+        symbol0={symbol0}
+        checkPairBalance={checkPairBalance}
+        checkPairAddress={checkPairAddress}
+        handlecheckPairs={this.handlecheckPairs}
+        metamaskAddress={metamaskAddress}
+        Lblocking={Lblocking}
+        Tblocking={Tblocking}
+      />
+    );
+  }
+}
+
+export default ProfileActionsContainer;
